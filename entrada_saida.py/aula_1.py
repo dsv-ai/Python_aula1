8891b0a6f0602faabc3a9dea4e0fa455b06d1d64
Davi Sosa Vieira <davisosavieira025@gmail.com>
2026-04-24 20:21:12 -0300

    exercicio-01: saudacao David Sosa

---
+++ entrada_saida.py/aula_1.py
@@ -0,0 +1,5 @@
+# Exiba em 3 linhas separadas: seu nome, sua cidade e seu curso. Cada informação em seu próprio print().
+
+print("Nome: David Sosa Vieira")
+print("Cidade: Areal/Arniqueiras")
+print("Curso: Técnico em Inteligência Artificial") # Criei cada print para chamar os dados, sugeridos no exercícios 
