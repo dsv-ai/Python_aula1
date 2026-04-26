5e4c89839c4d77cffd8d4079af3f51b53d23535d
Davi Sosa Vieira <davisosavieira025@gmail.com>
2026-04-25 21:27:43 -0300

    exercicio_04 realizado: Saudação Davi

---
+++ entrada_saida.py/aula_2.py
@@ -0,0 +1,9 @@
+# Peça o nome e a cidade do usuário. Exiba: "Bem-vindo(a), Ana! Você é de Curitiba." — adaptado aos dados digitados.
+
+
+print("Convite Para uma Festa Nerd!! ")
+
+nome = input("Digite seu nome: ")
+cidade = input("Digite sua Cidade: ")
+
+print(f"Obrido por ter vindo a minha Festa " + nome +  ", Poxa você é de ", cidade, "!!")
