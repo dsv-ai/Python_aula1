8d014ecda5f335ddfcd1b0f6cacb85da99acf8de
Davi Sosa Vieira <davisosavieira025@gmail.com>
2026-04-25 21:40:39 -0300

    exercicio_06 realizado: Saudação David

---
+++ entrada_saida.py/aula_3.py
@@ -0,0 +1,13 @@
+"""
+Leia a idade e calcule o ano de nascimento aproximado 
+(use 2025 como ano atual). Exiba: "Você nasceu por volta de 2000.
+
+"""
+
+print("Vamos descobrir qual ano você nasceu!!")
+
+ano = int(input("Quantos anos você tem! "))
+
+conta = (2026 - ano)
+
+print(f"O ano que você nasceu é {conta}! ")
